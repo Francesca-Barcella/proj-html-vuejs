392e0e83c1aa427499419833f8ee987249b98833
Francesca-Barcella <pollon.fra@gmail>
2022-11-22 21:04:00 +0100

    main - artciles&team - dynamic

--- src/components/main_components/ArticlesSection.vue
+++ src/components/main_components/ArticlesSection.vue
@@ -1,6 +1,29 @@
 <script>
 export default {
-    name: 'ArticlesSection'
+    name: 'ArticlesSection',
+    data(){
+        return{
+            imgArticles:[
+                {
+                    url: 'h3-img-1.jpg'
+                },
+                {
+                    url: 'h3-img-2.jpg'
+                },
+                {
+                    url: 'h3-img-3.jpg'
+                },
+                {
+                    url: 'h3-img-4.jpg'
+                }
+            ]
+        }
+    },
+    methods: {
+        getImageUrl(name) {
+            return new URL(`../../assets/img/${name}`, import.meta.url).href
+        }
+    }
 }
 </script>
 
@@ -8,10 +31,7 @@ export default {
     <section class="articles">
         <div class="container-fluid my-3">
             <div class="row">
-                <div class="col"><img src="../../assets/img/h3-img-1.jpg" alt=""></div>
-                <div class="col"><img src="../../assets/img/h3-img-2.jpg" alt=""></div>
-                <div class="col"><img src="../../assets/img/h3-img-3.jpg" alt=""></div>
-                <div class="col"><img src="../../assets/img/h3-img-4.jpg" alt=""></div>
+                <div class="col" v-for="item in imgArticles"><img :src="getImageUrl(item.url)" alt=""></div>
             </div>
         </div>
     </section>
@@ -20,11 +40,11 @@ export default {
 <style lang="scss" scoped>
 @use '../../assets/scss/partials/variables.scss' as *;
 
-.row{
-    justify-content: space-between;
-    /* margin-left: -1.5rem; */
+.col {
+    padding: 0;
 }
 .col>img:hover {
     cursor: pointer;
+    
 }
 </style>
--- src/components/main_components/OurTeamSection.vue
+++ src/components/main_components/OurTeamSection.vue
@@ -1,6 +1,29 @@
 <script>
 export default {
     name: 'OurTeamSection',
+    data(){
+        return{
+            teamAvatar:[
+                {
+                    url: 'h1-team-1a-700x700.jpg'
+                },
+                {
+                    url: 'h1-team-2a.jpg'
+                },
+                {
+                    url: 'h1-team-3a.jpg'
+                },
+                {
+                    url: 'h1-team-4a.jpg'
+                }
+            ]
+        }
+    },
+    methods: {
+        getImageUrl(name) {
+            return new URL(`../../assets/img/${name}`, import.meta.url).href
+        }
+    }
 }
 </script>
 
@@ -8,10 +31,8 @@ export default {
     <section class="our_team">
         <div class="container-fluid my-2">
             <div class="row">
-                <div class="col"> </div>
-                <div class="col"> </div>
-                <div class="col"> </div>
-                <div class="col"> </div>
+                <div class="col" v-for="item in teamAvatar"><img :src="getImageUrl(item.url)" alt=""></div>
+
             </div>
         </div>
     </section>
@@ -19,9 +40,7 @@ export default {
 
 <style lang="scss" scoped>
 @use '../../assets/scss/partials/variables.scss' as *;
-.col {
-    height: 200px;
-    background-color: $proj_primary;
-    border: 1px solid black;
+.col{
+    padding: 0;
 }
 </style>
